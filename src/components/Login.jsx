71d1091aa8dbb487cfd3450eaa1ce7b8c6aaa503
MaybeRosado <erosado@ucol.mx>
2024-11-29 09:05:04 -0600

    Deleted some style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -29,39 +29,39 @@ const Login = ({setUser}) => {
   };
   
   return (
-    <div className="padding-style">
+    <div >
       <h2>{isRegistering ? 'Register' : 'Login'}</h2>
       {!isRegistering ? (
         // Formulario de login
-        <form onSubmit={handleSubmit} className="padding-style">
+        <form onSubmit={handleSubmit} >
           <div>
             <label
-              className="padding-style"
+              
             >Email</label>
             <input 
               type="email" 
               value={email} 
               onChange={(e) => setEmail(e.target.value)} 
-              className="padding-style"
+              
               required 
             />
           </div>
           <div>
             <label
-              className="padding-style"
+              
             >Password</label>
             <input 
               type="password" 
               value={password} 
               onChange={(e) => setPassword(e.target.value)} 
               required 
-              className="padding-style"
+              
             />
           </div>
-          {error && <p className="padding-style">{error}</p>}
-          <button type="submit" className="padding-style">Login</button>
-          <p className="padding-style">Not registered?</p>
-          <button type="button" className="padding-style"onClick={toggleForm}>Create an account</button>
+          {error && <p >{error}</p>}
+          <button type="submit" >Login</button>
+          <p >Not registered?</p>
+          <button type="button" onClick={toggleForm}>Create an account</button>
         </form>
       ) : (
         // Formulario de registro
